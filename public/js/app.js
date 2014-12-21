dad9f6779b394e1f96971967e174e4c068ad27ca
Carlos Matheu Armengol <info@plastikaweb.com>
2014-12-21 17:38:01 +0100

    first commit

---
+++ public/js/app.js
@@ -0,0 +1,10 @@
+/**
+ * @fileOverview angularjs todosFactory.js
+ * @author info@plastikaweb.com (Carlos Matheu)
+ * API REST
+ */
+(function(){
+    "use strict";
+
+    var app = angular.module('mainApp', []);
+})();
